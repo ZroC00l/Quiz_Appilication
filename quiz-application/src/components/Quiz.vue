49303310e2c8ba58f89d83380a23809cf3c6b41d
ZroC00l <giftgabanakgosi@gmail.com>
2021-02-01 14:39:51 +0200

    setting up components

---
+++ quiz-application/src/components/Quiz.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="container">
+            <div v-if="Question.length">
+                <h1><span>Question:{{current + 1}}</span></h1>
+                <h2>{{Question[current].question}}</h2>
+            <div class="nav">
+                <button>Submit</button>
+                <button>Next</button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+    computed:{
+        ...mapState(['Question','current']),
+    },
+}
+</script>
