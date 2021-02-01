3d40ac637e0733c46c33f7504e3b086cd7de1cff
ZroC00l <giftgabanakgosi@gmail.com>
2021-02-01 14:25:10 +0200

    setting up components

--- quiz-application/src/store/index.js
+++ quiz-application/src/store/index.js
@@ -7,18 +7,32 @@ Vue.use(vue)
 export default new  Vuex.store({
 
     state:{
+        Question:[],
+        current:0,
         difficultyLevel: null
     },
 
     mutations:{
         SET_DIFFICULTY(state,difficulty){
             state.difficultyLevel = difficulty
+        },
+        SET_QUESTION(state,data){
+            state.Question = data
         }
+
     },
     
     actions:{
         beginQuiz:({commit},difficulty) => {
             commit('SET_DIFFICULTY',difficulty)
+            fetch(`https://opentdb.com/api.php?amount=10&category=31&difficulty=${state.difficultyLevel}&type=multiple`)
+                .then(response => response.json())
+                .then(data=>{
+                    commit('SET_QUESTION',data.results)
+                    console.log(state.Question)
+                })
+                .catch(error => console.log(error)) 
+                 
         }
     },       
 })
